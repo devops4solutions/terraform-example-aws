379d7176d33acb927239880babdd134c6f9ee91e
Nikhil Khandelwal <devops4solutions@gmail.com>
2020-09-25 15:12:03 -0400

    add code

--- modules/security_groups/security_groups.tf
+++ modules/security_groups/security_groups.tf
@@ -54,3 +54,13 @@ resource "aws_security_group" "Database_SG" {
   }
 }
 
+output "FrontEnd_SG_id"
+{
+    value = "${aws_security_group.FrontEnd_SG.id}"
+}
+
+output "Database_SG_id"
+{
+    value = "${aws_security_group.Database_SG.id}"
+
+}
