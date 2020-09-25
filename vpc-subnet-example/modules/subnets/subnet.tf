73bc48e12fdab18d2f20ccda374522aa0a26590c
Nikhil Khandelwal <devops4solutions@gmail.com>
2020-09-24 21:06:39 -0400

    initial commit

--- vpc-subnet-example/modules/subnets/subnet.tf
+++ vpc-subnet-example/modules/subnets/subnet.tf
@@ -2,7 +2,7 @@
 
 resource "aws_subnet" "PublicAZA" {
   vpc_id = var.vpc_id
-  cidr_block = "${var.Subnet-Public-AzA-CIDR}"
+  cidr_block = var.Subnet-Public-AzA-CIDR
   tags = {
         Name = "PublicSubnetAZA"
   }
