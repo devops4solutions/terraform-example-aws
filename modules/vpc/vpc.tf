649aa5a0ec20a47faec2a39a703f8cbee26ec957
Nikhil Khandelwal <devops4solutions@gmail.com>
2020-09-25 15:43:24 -0400

    add code

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -8,3 +8,14 @@ enable_dns_support = true
   }
 }
 
+resource "aws_flow_log" "vpc_flow_log" {
+  log_group_name = aws_cloudwatch_log_group.vpc_cloudwatch_log_group.name
+ iam_role_arn   = var.vpcflowlogsrole_arn
+  vpc_id         = aws_vpc.terraform_vpc.id
+  traffic_type   = "ALL"
+}
+
+resource "aws_cloudwatch_log_group" "vpc_cloudwatch_log_group" {
+  name = "vpc_cloudwatch_log_group"
+}
+
