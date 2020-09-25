bfc54cdb96939d71506011ad3f2691cf6af9a421
Nikhil Khandelwal <devops4solutions@gmail.com>
2020-09-25 15:19:50 -0400

    add code

--- modules/security_groups/outputs.tf
+++ modules/security_groups/outputs.tf
@@ -2,6 +2,6 @@ output "FrontEnd_SG_id" {
  value = "${aws_security_group.FrontEnd_SG.id}"
 }
 
-output "private_subnet_id" {
+output "Database_SG_id" {
  value = "${aws_security_group.Database_SG.id}"
 }
